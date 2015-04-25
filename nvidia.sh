8a2b794291e7dc18c43c3ed803aaaaeb5ed1b974
Diamond Bond <diamond.nigel75@Gmail.com>
2015-04-25 12:47:45 +0530

    Update nvidia.sh

--- nvidia.sh
+++ nvidia.sh
@@ -1,5 +1,5 @@
 # <INIT>
-target=ubuntu_gnome_x64
+target=ubuntu_x64
 clear
 
 # <ADD REPOS>
@@ -10,17 +10,18 @@ sudo apt-get update
 
 # <INSTALL>
 sudo apt-get install bumblebee #Bumblebee 
-sudo apt-get install nvidia-331 #Nvidia Drivers
+sudo apt-get install nvidia-331 #Nvidia Drivers, Nvidia Settings & Primus
 
 # <EXTRA>
 #Config:
 echo "and add the modify as follows:
 [bumblebee] section: after "Driver=" insert "nvidia"
 [nvidia-driver] section: after "KernelDriver=" insert "nvidia-XXX"
-[nvidia-driver] section: after "LibraryPath=" insert "/usr/lib/nvidia-XXX, /usr/lib/nvidia-331-updates:/usr/lib32/nvidia-XXX"
+[nvidia-driver] section: after "LibraryPath=" insert "/usr/lib/nvidia-XXX, /usr/lib/nvidia-XXX:/usr/lib32/nvidia-XXX"
 [nvidia-driver] section: after "XorgModulePath=" insert "/usr/lib/nvidia-XXX/xorg,/usr/lib/xorg/modules"
-Where XXX is the nvidia driver you have installed, in the example: nvidia-331.
-In the terminal, nano should have started, go and edit as said now." >> ~/Desktop/nvoptions.txt
+Where XXX is the nvidia driver you have installed, for example: nvidia-331.
+In the terminal, nano should have started, go and edit as said above now.
+When finished press CTRL+O and ENTER to save then CTRL+X to exit." >> ~/Desktop/nvoptions.txt
 #Show Config
 xdg-open ~/Desktop/nvoptions.txt
 #Edit
