95ac64d8be97ee0211a6a3b13b5c26f6deb830e1
Diamond Bond <diamond.nigel75@Gmail.com>
2015-04-26 12:05:03 +0530

    Update update.sh

--- update.sh
+++ update.sh
@@ -38,6 +38,8 @@ sudo apt-get install filezilla #FTP
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 rm -f google-chrome-stable_current_amd64.deb
+#Use the force luke!
+sudo apt-get -f install
 
 # <SETUP>
 #Bash Aliases
