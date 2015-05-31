87b0d25a0580d53a0051490d9a09fb53cd40748d
Diamond Bond <diamond.nigel75@Gmail.com>
2015-05-31 07:07:03 +0530

    cleanup
    Signed-off-by: Diamond Bond <diamond.nigel75@Gmail.com>

--- dev_init.sh
+++ dev_init.sh
@@ -9,9 +9,10 @@ sudo add-apt-repository -y ppa:webupd8team/java
 sudo apt-get -y update
 
 # <INSTALL>
-sudo apt-get -y install oracle-java7-installer
-sudo apt-get install git git-core gnupg ccache lzop flex bison gperf build-essential zip curl zlib1g-dev zlib1g-dev:i386 libc6-dev lib32ncurses5 lib32z1 lib32bz2-1.0 lib32ncurses5-dev x11proto-core-dev libx11-dev:i386 libreadline6-dev:i386 lib32z-dev libgl1-mesa-glx:i386 libgl1-mesa-dev g++-multilib mingw32 tofrodos python-markdown libxml2-utils xsltproc readline-common libreadline6-dev libreadline6 lib32readline-gplv2-dev libncurses5-dev lib32readline5 lib32readline6 libreadline-dev libreadline6-dev:i386 libreadline6:i386 bzip2 libbz2-dev libbz2-1.0 libghc-bzlib-dev lib32bz2-dev libsdl1.2-dev libesd0-dev squashfs-tools pngcrush schedtool libwxgtk2.8-dev python
-sudo ln -s /usr/lib/i386-linux-gnu/mesa/libGL.so.1 /usr/lib/i386-linux-gnu/libGL.so
+#sudo apt-get -y install oracle-java7-installer
+sudo apt-get install git git-core gnupg ccache lzop flex bison gperf build-essential zip curl g++-multilib tofrodos python-markdown xsltproc readline-common bzip2 squashfs-tools pngcrush schedtool python
+bison g++-multilib git gperf libxml2-utils make zlib1g-dev:i386 zip
+#sudo ln -s /usr/lib/i386-linux-gnu/mesa/libGL.so.1 /usr/lib/i386-linux-gnu/libGL.so
 
 # <MORE INSTALL>
 mkdir ~/bin && curl http://commondatastorage.googleapis.com/git-repo-downloads/repo > ~/bin/repo && chmod a+x ~/bin/repo
